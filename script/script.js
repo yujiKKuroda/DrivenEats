8e6271fcf0a3b6810585e493a28041bb691d1fe3
RodrigoYujiKogaKuroda <yujikuroda.rykk@gmail.com>
2022-06-13 17:21:07 -0300

    Edição de classes para coletar variáveis globais para o WhatsApp

--- script/script.js
+++ script/script.js
@@ -1,20 +1,24 @@
 let prato = 0;
 let nomePrato = "";
+let precoPrato = "";
 let bebida = 0;
 let nomeBebida = "";
+let precoBebida = "";
 let sobremesa = 0;
 let nomeSobremesa = "";
+let precoSobremesa = "";
 
 function selectPratos (item) {
     if (prato <= 0){
         prato = 1;
     }
     const itemClicado = document.querySelector(".pratos .selecionado");
-    nomePrato = document.querySelector(".pratos .selecionado .produto").innerHTML;
     if (itemClicado !== null) {
         itemClicado.classList.remove("selecionado");
     }
     item.classList.add("selecionado");
+    nomePrato = document.querySelector(".pratos .selecionado .produto").innerHTML;
+    precoPrato = document.querySelector(".pratos .selecionado .preco p").innerHTML;
 }
 
 function selectBebidas (item) {
@@ -26,6 +30,8 @@ function selectBebidas (item) {
         itemClicado.classList.remove("selecionado");
     }
     item.classList.add("selecionado");
+    nomeBebida = document.querySelector(".bebidas .selecionado .produto").innerHTML;
+    precoBebida = document.querySelector(".bebidas .selecionado .preco p").innerHTML;
 }
 
 function selectSobremesas (item) {
@@ -37,6 +43,8 @@ function selectSobremesas (item) {
         itemClicado.classList.remove("selecionado");
     }
     item.classList.add("selecionado");
+    nomeSobremesa = document.querySelector(".sobremesas .selecionado .produto").innerHTML;
+    precoSobremesa = document.querySelector(".sobremesas .selecionado .preco p").innerHTML;
 }
 
 function marcar() {
